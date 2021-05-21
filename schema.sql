d64614c710972d8cc2e656ca9764457c0f3dfdea
Lucas Munson <lucasbmunson@gmail.com>
2021-05-21 16:04:08 -0400

    add schema

---
+++ schema.sql
@@ -0,0 +1,77 @@
+--create departments table 
+
+DROP TABLE departments;
+
+CREATE TABLE departments(
+    dept_no VARCHAR(30),
+    dept_name VARCHAR(30),
+    PRIMARY KEY (dept_no)
+);
+
+SELECT * FROM departments
+
+--create dept_emp table
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp ( 
+    emp_no INT,
+    dept_no VARCHAR(30)
+);
+
+SELECT * FROM dept_emp;
+-- dept_manager
+
+DROP TABLE dept_manager;
+
+CREATE TABLE dept_manager(
+    dept_no VARCHAR(30),
+	emp_no INT,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+select * from dept_manager;
+
+--create employees table
+
+DROP TABLE employees;
+
+CREATE table employees (
+    emp_no INT,
+	emp_title_id VARCHAR(30),
+    birth_date DATE,
+    first_name VARCHAR(30) ,
+    last_name VARCHAR(30),
+    sex VARCHAR(2), 
+    hire_date DATE,
+    PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM employees;
+
+-- salareis table. 
+
+drop table salaries;
+
+CREATE TABLE salaries(
+    emp_no INT,
+    salary INT,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries;
+
+--titles 
+
+DROP TABLE titles;
+
+CREATE TABLE titles(
+    title_id VARCHAR(30),
+    title VARCHAR(30)
+);
+
+select * from titles; 
+
+
+
